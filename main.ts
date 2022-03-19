313ccd3d9a552e0b062b0bdff97393484e0b9716
texnic <kotya.karapetyan@gmail.com>
2022-03-19 20:52:10 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onSound(DetectedSound.Loud, function () {
+    index += -1
+    if (index == -1) {
+        index = 3
+    }
+})
+let index = 0
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+index = 3
+basic.forever(function () {
+    if (index == 3) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+    if (index == 2) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+    if (index == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+    if (index == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
